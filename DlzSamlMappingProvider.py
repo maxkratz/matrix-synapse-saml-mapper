08f9c2c539f233da0ea705836fc16618be4f98a2
Maximilian Kratz <account@maxkratz.com>
2020-10-23 17:03:28 +0200

    Some Snapshot before breaking everything.

--- DlzSamlMappingProvider.py
+++ DlzSamlMappingProvider.py
@@ -1,6 +1,21 @@
-
+import attr
+import saml2
+import saml2.response
+from typing import Dict, Set, Tuple
+# from synapse.config import ConfigError
 
 # See https://github.com/matrix-org/synapse/blob/master/docs/sso_mapping_providers.md
+
+
+@attr.s
+class SamlConfig:
+    mxid_source_attribute = attr.ib()
+    mxid_mapper = attr.ib()
+
+
+class MappingException(Exception):
+    """Used to catch errors when mapping the SAML2 response to a user."""
+
 
 class DlzSamlMappingProvider:
     __author__ = "Maximilian Kratz"
@@ -9,15 +24,17 @@ class DlzSamlMappingProvider:
     __license__ = "I hate the HRZ for not providing displayName - License"
     __status__ = "Development"
 
-    def __init__(self, parsed_config):
+    def __init__(self, parsed_config: SamlConfig):
         """
         Args:
             parsed_config - A configuration object that is the return value of the parse_config 
                 method. You should set any configuration options needed by the module here.
         """
+        self._mxid_source_attribute = parsed_config.mxid_source_attribute
+        self._mxid_mapper = parsed_config.mxid_mapper
 
     @staticmethod
-    def parse_config(config):
+    def parse_config(config: dict) -> SamlConfig:
         """
         Args:
             config: A dict representing the parsed content of the 
@@ -27,9 +44,24 @@ class DlzSamlMappingProvider:
         Returns:
             SamlConfig: A custom config object for this module
         """
+        # Parse config options and use defaults where necessary
+        mxid_source_attribute = config.get("mxid_source_attribute", "uid")
+        mapping_type = config.get("mxid_mapping", "hexencode")
+
+        # Retrieve the associating mapping function
+        # try:
+        #     mxid_mapper = MXID_MAPPER_MAP[mapping_type]
+        # except KeyError:
+        #     raise ConfigError(
+        #         "saml2_config.user_mapping_provider.config: '%s' is not a valid "
+        #         "mxid_mapping value" % (mapping_type,)
+        #     )
+
+        # return SamlConfig(mxid_source_attribute, mxid_mapper)
+        return SamlConfig(mxid_source_attribute, None)
 
     @staticmethod
-    def get_saml_attributes(config):
+    def get_saml_attributes(config: SamlConfig) -> Tuple[Set[str], Set[str]]:
         """
         Args:
             config: A object resulting from a call to parse_config.
@@ -39,19 +71,31 @@ class DlzSamlMappingProvider:
                 second set consists of those attributes which can be used if
                 available, but are not necessary
         """
+        return {"uid", config.mxid_source_attribute}, {"displayName", "email"}
 
-    def get_remote_user_id(self, saml_response, client_redirect_url):
+    def get_remote_user_id(
+            self, saml_response: saml2.response.AuthnResponse, client_redirect_url: str
+    ) -> str:
         """
         Args:
             saml_response: A saml2.response.AuthnResponse object to extract user information from.
             client_redirect_url: A string, the URL that the client will be redirected to.
         """
+        try:
+            return saml_response.ava["uid"][0]
+        except KeyError:
+            raise MappingException("'uid' not in SAML2 response")
 
-    def saml_response_to_user_attributes(self, saml_response, failures, client_redirect_url):
+    def saml_response_to_user_attributes(
+        self,
+        saml_response,
+        failures,
+        client_redirect_url
+    ) -> dict:
         """
         Args:
             saml_response: A saml2.response.AuthnResponse object to extract user information from.
-                failures: An int that represents the amount of times the returned mxid localpart 
+            failures: An int that represents the amount of times the returned mxid localpart
                 mapping has failed. This should be used to create a deduplicated mxid localpart
                 which should be returned instead. For example, if this method returns john.doe as
                 the value of mxid_localpart in the returned dict, and that is already taken on the
@@ -65,3 +109,36 @@ class DlzSamlMappingProvider:
                 * displayname (str): The displayname of the user
                 * emails (list[str]): Any emails for the user
         """
+        try:
+            mxid_source = saml_response.ava[self._mxid_source_attribute][0]
+        except KeyError:
+            # logger.warning(
+            #     "SAML2 response lacks a '%s' attestation", self._mxid_source_attribute,
+            # )
+            # raise SynapseError(
+            #     400, "%s not in SAML2 response" % (self._mxid_source_attribute,)
+            # )
+            raise AttributeError(
+                400, "%s not in SAML2 response" % (self._mxid_source_attribute,)
+            )
+
+            # Use the configured mapper for this mxid_source
+        base_mxid_localpart = self._mxid_mapper(mxid_source)
+
+        # Append suffix integer if last call to this function failed to produce
+        # a usable mxid
+        localpart = base_mxid_localpart + (str(failures) if failures else "")
+
+        # Retrieve the display name from the saml response
+        # If displayname is None, the mxid_localpart will be used instead
+        displayname = saml_response.ava.get("displayName", [None])[0]
+
+        # Retrieve any emails present in the saml response
+        emails = saml_response.ava.get("email", [])
+
+        return {
+            "mxid_localpart": localpart,
+            "displayname": displayname,
+            "emails": emails,
+        }
+
